010e028abb479372345b487538057f7ae0efc260
Ronaldo <ronaldojssilva@gmail.com>
2025-05-03 17:08:59 -0300

    feat(data): criar repositorios, contexts,...

---
+++ src/PedidosService.Infrastructure/DbContexts/Mappings/ItemPedidoMap.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using PedidosService.Domain.Entities;
+
+namespace PedidosService.Infrastructure.DbContexts.Mappings
+{
+    public class ItemPedidoMap : IEntityTypeConfiguration<ItemPedido>
+    {
+        public void Configure(EntityTypeBuilder<ItemPedido> builder)
+        {
+            builder.ToTable("ItensPedido");
+
+            builder.HasKey(i => i.Id);
+            builder.Property(p => p.Id).ValueGeneratedOnAdd();
+
+            builder.HasOne(p => p.Pedido)
+                .WithMany(p => p.Itens)
+                .HasForeignKey(p => p.PedidoId) 
+                .IsRequired();
+
+            builder.Property(i => i.ProdutoId)
+                .IsRequired();
+
+            builder.Property(i => i.Quantidade)
+                .IsRequired();
+
+            builder.Property(i => i.ValorUnitario)
+                .HasColumnType("decimal(18,2)")
+                .IsRequired();
+        }
+    }
+}
---
+++ src/PedidosService.Infrastructure/DbContexts/Mappings/PedidoMap.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using PedidosService.Domain.Entities;
+
+namespace PedidosService.Infrastructure.DbContexts.Mappings
+{
+    public class PedidoMap : IEntityTypeConfiguration<Pedido>
+    {
+        public void Configure(EntityTypeBuilder<Pedido> builder)
+        {
+
+            builder.ToTable("Pedidos");
+
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Id).ValueGeneratedOnAdd();
+
+            builder.Property(p => p.PedidoId).IsRequired();
+
+            builder.Property(p => p.ClienteId).IsRequired();
+
+            builder.HasMany(p => p.Itens)
+                   .WithOne(i => i.Pedido)
+                   .HasForeignKey(i => i.PedidoId)
+                   .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Property(p => p.Status)
+                .HasConversion<int>()
+                .IsRequired();
+
+            builder.Property(p => p.CriadoEm)
+                .IsRequired();
+
+            builder.Property(i => i.TotalImpostos)
+                .HasColumnType("decimal(18,2)")
+            .IsRequired();
+
+            builder.Property(p => p.EnviadoSistemaB)
+                .HasDefaultValue(false);
+
+            builder.Navigation(p => p.Itens).AutoInclude();
+        }
+    }
+}
